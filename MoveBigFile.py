6f1bb30bc1ed16a0ac14af86aa6d31bb1badd084
ericwang <wwssllabcd@gmail.com>
2017-10-04 22:22:11 +0800

    update code

--- MoveBigFile.py
+++ MoveBigFile.py
@@ -1,28 +1,19 @@
-#-*- coding: utf-8 -*-　
+# -*- coding: UTF-8 -*-
+
 import os
 import shutil
 
-
-
 def move_big_files(fpath):
-    """Docstring"""
-    # dirPath: 目前走訪的目錄
-    # dirs: 該path所包含的所有的目錄
-    # files: 該path 所包含的檔案
-    # os.walk: 會從起始位置往下走訪每個dirPath
-    for dirPath, dirs, files in os.walk(fpath):
-        if dirPath == fpath:
+    for root, dirs, files in os.walk(fpath):
+        if root == fpath:
             continue
         for f in files:
-            fp = dirPath + "/" + f
-            fsizeMb = os.path.getsize(fp) / 1024 / 1024
+            fp = os.path.join(root, f)
+            fsizeMb = os.path.getsize(fp) /1024/1024
             if fsizeMb > 80:
                 shutil.move(fp, fpath)
-                print "move " + fp
+                print ("move " + fp)
 
-    
-
-
-FOLDER_PATH = './'
+FOLDER_PATH = './'.encode('utf-8')
 move_big_files(FOLDER_PATH)
-print "Finish"
+print ("Finish")
