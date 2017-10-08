a287806cd789032a7bb3de2eb297aa1204911900
ericwang <wwssllabcd@gmail.com>
2017-10-09 02:02:45 +0800

    add print f

--- MoveBigFile.py
+++ MoveBigFile.py
@@ -7,14 +7,16 @@ import shutil
 def move_big_files(src, dst):
     fsizeMb = os.path.getsize(src)/1024/1024
     if fsizeMb > 100:
+        print(src.encode("utf-8"))
         shutil.move(src,  dst)
         print ("move " + src)
 
-def move_big_files(fpath, execFun):
+def get_file_in_fir(fpath, execFun):
     for root, dirs, files in os.walk(fpath):
         if root == fpath:
             continue
         for f in files:
+           
             fp = os.path.join(root, f)
             execFun(fp, fpath)
             
@@ -22,6 +24,6 @@ def move_big_files(fpath, execFun):
 
 
 FOLDER_PATH = './'
-move_big_files(FOLDER_PATH, move_big_files)
-print ("Finish")
+get_file_in_fir(FOLDER_PATH, move_big_files)
+print("Finish")
 os.system("pause")
